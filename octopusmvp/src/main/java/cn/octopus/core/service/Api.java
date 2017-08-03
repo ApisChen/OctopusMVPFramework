7329e0420fc46bc36e91649742057fd64dd4967d
Lei Weibo <leiwb@tradenb.com>
2017-08-03 23:28:17 +0800

    refector the Api.java

--- octopusmvp/src/main/java/cn/octopus/core/service/Api.java
+++ octopusmvp/src/main/java/cn/octopus/core/service/Api.java
@@ -1,16 +1,13 @@
 package cn.octopus.core.service;
 
-import cn.octopus.core.utils.ClazzUtil;
 import com.google.gson.GsonBuilder;
 
 import java.io.IOException;
-import java.lang.reflect.InvocationTargetException;
-import java.lang.reflect.ParameterizedType;
-import java.lang.reflect.Type;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.concurrent.TimeUnit;
 
 import cn.octopus.core.BuildConfig;
-import cn.octopus.core.base.BaseApplication;
 import io.reactivex.schedulers.Schedulers;
 import okhttp3.Interceptor;
 import okhttp3.OkHttpClient;
@@ -21,83 +18,100 @@ import retrofit2.Retrofit;
 import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory;
 import retrofit2.converter.gson.GsonConverterFactory;
 
-
 /**
- * Api service
+ * Api service.
+ * Normally, this class will be initialized in app's Application only once. But, sometimes, if there
+ * are more than one server for the app, you can initialized the Api class with specify host url
+ * more than one.
+ *
  * Created by JieGuo on 2017/3/16.
  */
 
-public abstract class Api<T> {
-    private static final String PLATFORM = "android";
-    private static final String CLIENT = "octopus-mvp-client";
+public class Api {
 
-    private T apiService;
-    private Class<T> clazzOfT;
+  private Map<Class, Object> serviceMap = new HashMap<>();
+  private static final String PLATFORM = "android";
+  private static final String CLIENT = "octopus-mvp-client";
+  private String hostUrl;
+  private Retrofit retrofit;
 
-    protected Api() {
-        OkHttpClient.Builder httpClientBuilder = new OkHttpClient.Builder();
-        httpClientBuilder.connectTimeout(10, TimeUnit.SECONDS);
-        httpClientBuilder.writeTimeout(30, TimeUnit.SECONDS);
-        httpClientBuilder.readTimeout(30, TimeUnit.SECONDS);
+  public Api(String hostUrl) {
+    this.hostUrl = hostUrl;
+    this.retrofit = getRetrofit();
+  }
 
-        httpClientBuilder.networkInterceptors()
-                .add(new Interceptor() {
-                    @Override
-                    public Response intercept(Chain chain) throws IOException {
-                        Request.Builder req = chain.request().newBuilder();
-                        req.addHeader("Accept-Charset", "utf-8");
-                        req.addHeader("Content-Type", "application/x-www-form-urlencoded; charset=UTF-8")
-                                .addHeader("Connection", "keep-alive")
-                                .addHeader("Accept", "*/*")
-                                .addHeader("x-version", BuildConfig.VERSION_NAME)
-                                .addHeader("x-platform", PLATFORM)
-                                .addHeader("x-client", CLIENT);
+  /**
+   * Get the Retrofit object base on the specified host url.
+   *
+   * @return Retrofit object.
+   */
+  private Retrofit getRetrofit() {
+    OkHttpClient.Builder httpClientBuilder = new OkHttpClient.Builder();
+    httpClientBuilder.connectTimeout(10, TimeUnit.SECONDS);
+    httpClientBuilder.writeTimeout(30, TimeUnit.SECONDS);
+    httpClientBuilder.readTimeout(30, TimeUnit.SECONDS);
 
-                        addAuthorizationHeader(req);
-                        Response response = chain.proceed(req.build());
-                        return response.newBuilder().build();
-                    }
-                });
-        httpClientBuilder.addInterceptor(
-                new HttpLoggingInterceptor().setLevel(
-                        BuildConfig.DEBUG ? HttpLoggingInterceptor.Level.BODY : HttpLoggingInterceptor.Level.NONE
-                )
-        );
+    httpClientBuilder.networkInterceptors().add(new Interceptor() {
+      @Override public Response intercept(Chain chain) throws IOException {
+        Request.Builder req = chain.request().newBuilder();
+        req.addHeader("Accept-Charset", "utf-8");
+        req.addHeader("Content-Type", "application/x-www-form-urlencoded; charset=UTF-8")
+            .addHeader("Connection", "keep-alive")
+            .addHeader("Accept", "*/*")
+            .addHeader("x-version", BuildConfig.VERSION_NAME)
+            .addHeader("x-platform", PLATFORM)
+            .addHeader("x-client", CLIENT);
 
+        addAuthorizationHeader(req);
+        Response response = chain.proceed(req.build());
+        return response.newBuilder().build();
+      }
+    });
+    httpClientBuilder.addInterceptor(new HttpLoggingInterceptor().setLevel(
+        BuildConfig.DEBUG ? HttpLoggingInterceptor.Level.HEADERS : HttpLoggingInterceptor.Level.NONE));
 
-        Retrofit.Builder retrofitBuilder = new Retrofit.Builder();
+    Retrofit.Builder retrofitBuilder = new Retrofit.Builder();
 
-        GsonBuilder gsonBuilder = new GsonBuilder();
+    GsonBuilder gsonBuilder = new GsonBuilder();
 
-        Retrofit retrofit = retrofitBuilder
-                .baseUrl(BaseApplication.getInstance().getBaseUrl())
-                .client(httpClientBuilder.build())
-                .addCallAdapterFactory(RxJava2CallAdapterFactory.createWithScheduler(Schedulers.io()))
-                .addConverterFactory(GsonConverterFactory.create(gsonBuilder.create()))
-                .build();
+    Retrofit retrofit = retrofitBuilder.baseUrl(hostUrl)
+        .client(httpClientBuilder.build())
+        .addCallAdapterFactory(RxJava2CallAdapterFactory.createWithScheduler(Schedulers.io()))
+        .addConverterFactory(GsonConverterFactory.create(gsonBuilder.create()))
+        .build();
 
-        clazzOfT = ClazzUtil.getClassOfT(getClass());
+    return retrofit;
+  }
 
-        apiService = createApiService(retrofit);
+  /**
+   * add token here.
+   *
+   * @param req request
+   */
+  protected void addAuthorizationHeader(Request.Builder req) {
+    //        if (!TextUtils.isEmpty(FastData.getToken())) {
+    //            String authorizationHeader = String.format(Locale.CHINA, "Bearer %s", FastData.getToken());
+    //            req.addHeader("Authorization", authorizationHeader);
+    //        }
+  }
+
+  /**
+   * Get the retrofit service class by the xxApi class. If the class is initialized, just get
+   * it from the cached instance; Or else, create a new instance via the newInstance() method.
+   *
+   * @param clazzOfService retrofit service interface
+   * @param <T> Type of retrofit service interface
+   * @return the Retrofit service instance which created by retrofit.
+   */
+  public <T> T getRetrofitService(Class clazzOfService) {
+    T t = (T) (serviceMap.get(clazzOfService));
+    if (t == null) {
+      t = (T) retrofit.create(clazzOfService);
+      serviceMap.put(clazzOfService, retrofit.create(clazzOfService));
     }
-
-    protected T createApiService(Retrofit retrofit) {
-        return retrofit.create(clazzOfT);
+    if (t == null) {
+      throw new RuntimeException("can not get the Api class ");
     }
-
-    /**
-     * add token here.
-     * @param req request
-     */
-    protected void addAuthorizationHeader(Request.Builder req) {
-//        if (!TextUtils.isEmpty(FastData.getToken())) {
-//            String authorizationHeader = String.format(Locale.CHINA, "Bearer %s", FastData.getToken());
-//            req.addHeader("Authorization", authorizationHeader);
-//        }
-    }
-
-    public T getService() {
-        return apiService;
-    }
-
+    return t;
+  }
 }
